78bde12f73721f2eabcd7629530a38a12a92d951
GonzaloOrellanaC <gonzalo.orellana@kahuel.com>
2022-02-16 11:44:49 -0300

    errores menores

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -9,16 +9,16 @@ const Footer = () => {
                     <div className='col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4'>
                         <dl className="list-footer">
                             <dt>
-                                <a href="">{t('homePage:title')}</a>
+                                <a href="/">{t('homePage:title')}</a>
                             </dt>
                             <dt>
-                                <a href="">{t('registerPage:title')}</a>
+                                <a href={t('routes:register')}>{t('registerPage:title')}</a>
                             </dt>
                             <dt>
-                                <a href="">{t('aboutPage:title')}</a>
+                                <a href={t('routes:about')}>{t('aboutPage:title')}</a>
                             </dt>
                             <dt>
-                                <a href="">{t('contactPage:title')}</a>
+                                <a href={t('routes:contact')}>{t('contactPage:title')}</a>
                             </dt>
                         </dl>
                     </div>
